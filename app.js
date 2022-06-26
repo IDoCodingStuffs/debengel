ee28adc5c4e6ba978782eb6f311506cd88d9c3c6
the-layman <sahinvictor@gmail.com>
2022-06-26 10:33:18 -0400

    Added blocked users retrieval

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+susersToHideFromDEBE = new Set();
+
+fetch('https://eksisozluk.com/takip-engellenmis').then(r => r.text()).then(result => {
+    console.log("--------------");
+    var div = document.createElement('div');
+    div.innerHTML = result;
+    
+    var lists = div.getElementsByClassName("relation-list");
+
+    var blockedList = lists[1].getElementsByTagName("li");
+    var blockedThreadsList = lists[2].getElementsByTagName("li");
+
+    for (let e of blockedList) susersToHideFromDEBE.add(e.getElementsByTagName("a")[1].getAttribute("data-nick"));
+    for (let e of blockedThreadsList) susersToHideFromDEBE.add(e.getElementsByTagName("a")[1].getAttribute("data-nick"));
+
+    console.log(Array.from(susersToHideFromDEBE)[0]);
+
+    console.log("--------------");
+});
